70420bb506f4cfe5707bf90f3c9fcbafdb18231c
Nyxion <raggar_tobba_92@hotmail.com>
2022-05-04 02:21:50 +0200

    https://www.codewars.com/kata/5861d28f124b35723e00005e

--- codewarsfunc.cpp
+++ codewarsfunc.cpp
@@ -48,3 +48,29 @@ std::vector<std::string> CodewarsFunc::string_to_array(const std::string& s)
 	test_array.push_back(s.substr(pos_start));
 	return string_array;
 }
+
+bool CodewarsFunc::zero_fuel(uint32_t distance_to_pump, uint32_t mpg, uint32_t fuel_left)
+{
+	/*
+	You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! 
+	You know that on average, your car runs on about 25 miles per gallon. 
+	There are 2 gallons left. Considering these factors, write a function that tells you if it is possible to get to the pump or not. 
+	Function should return true (1 in Prolog, NASM and COBOL) if it is possible and false (0 in Prolog, NASM and COBOL) if not. 
+	The input values are always positive.
+	*/
+	/*
+		how many miles per gallon goes into distance_to_pump if greater than fuel_left then false else true	
+	*/
+	uint32_t fuel_used = 0;
+	uint32_t dtp = distance_to_pump;
+	uint32_t miles_p_gallon = mpg;
+	while (fuel_left != 0)
+	{
+		dtp = dtp - mpg;
+		fuel_used++;
+		fuel_left--;
+	}
+	if (dtp > 0)
+		return false;
+	return true;
+}
--- codewarsfunc.h
+++ codewarsfunc.h
@@ -6,9 +6,12 @@
 class CodewarsFunc
 {
 public:
+	// Fields
 	std::vector<std::string> test_array;
+	// Methods
 	std::string bmi(double w, double h);
 	std::string number_to_String(int num);
 	std::vector<std::string> string_to_array(const std::string& s);
+	bool zero_fuel(uint32_t distance_to_pump, uint32_t mpg, uint32_t fuel_left);
 };
 
